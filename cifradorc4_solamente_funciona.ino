a833ee482b0e4f4200b8b482197b759ef280e685
Argonautex <74357760+Argonautex@users.noreply.github.com>
2021-07-08 22:23:04 +0200

    Update cifradorc4_solamente_funciona.ino

--- cifradorc4_solamente_funciona.ino
+++ cifradorc4_solamente_funciona.ino
@@ -1,82 +1,67 @@
-
 #include <SpritzCipher.h>
 
-
-
-
-String datos = "data,cansat";
-byte testMsg[30];
-const byte testKey[3] = { 0x00, 0x01, 0x02 };
-
-
-void testFunc(const byte *msg, byte msgLen, const byte *key, byte keyLen)
-{
-  spritz_ctx s_ctx;
-  byte buf[8]; 
-  unsigned int i;
-
-
-  for (i = 0; i < msgLen; i++) {
-    Serial.write(msg[i]);
-  }
-  Serial.println();
-
-
-  for (i = 0; i < keyLen; i++) {
-    if (key[i] < 0x10) { 
-      Serial.write('0');
-    }
-    Serial.print(key[i], HEX);
-  }
-  Serial.println();
-
-  spritz_setup(&s_ctx, key, keyLen);
-  spritz_crypt(&s_ctx, msg, msgLen, buf);
-
-
-  for (i = 0; i < msgLen; i++) {
-    if (buf[i] < 0x10) { 
-      Serial.write('0');
-    }
-    Serial.print(buf[i], HEX);
-  }
-  Serial.println();
-
-  spritz_setup(&s_ctx, key, keyLen);
-  spritz_crypt(&s_ctx, buf, msgLen, buf);
-
-
-  for (i = 0; i < msgLen; i++) {
-    Serial.write(buf[i]);
-  }
-  Serial.println();
-
-
-  if (spritz_compare(buf, msg, msgLen)) {
-
-    digitalWrite(LED_BUILTIN, HIGH); 
-    Serial.println("\n** WARNING: Output != Test_Vector **");
-  }
-  Serial.println();
-}
+//Declaramos la clave de cifrado
+char testKey[3] = { 0x00, 0x01, 0x02 };
 
 void setup() {
-
   Serial.begin(9600);
-  while (!Serial) {
-    ; 
-  }
-datos.getBytes(testMsg, 30);
-Serial.println(testMsg[0]);  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void loop() {
-  Serial.println("[Spritz library encryption/decryption function test]\n");
 
+  //Declaramos la string de datos del cansat
+  String datos = "data,cansat";
 
-  testFunc(testMsg, sizeof(testMsg), testKey, sizeof(testKey));
+  //Declaramos la string de los datos cifrados que enviaremos por LoRa
+  String datos_cif;
 
-  delay(5000); 
+  //Declaramos la string de los datos descifrados
+  String datos_plain;
+  
+  //Declaramos el tamaño de la cadena de datos del cansat
+  int leng = datos.length() + 1;
+
+  //Declaramos el buffer donde se guardaran los datos para cifar
+  char testMsg[leng];
+
+  // Pasamos la string de datos a bytes y lo guardamos en el buffer
+  datos.toCharArray(testMsg, leng);
+
+  //Llamamos a la funcion que va a cifrar/descifrar los datos
+  testFunc(testMsg, sizeof(testMsg), testKey, sizeof(testKey), datos_cif);
+
+  delay(5000);
   Serial.println();
+}
+void testFunc(const byte *msg, byte msgLen, const byte *key, byte keyLen, String datos_cif)
+{
+  spritz_ctx s_ctx;
+
+  //Declaramos el output buffer de cifrado
+  char buf[50];
+  char bufcif[50];
+  unsigned int i;
+
+  //Ciframos los datos del cansat
+  spritz_setup(&s_ctx, key, keyLen);
+  spritz_crypt(&s_ctx, msg, msgLen, buf);
+
+  for (i = 0; i < msgLen; i++) {
+    datos_cif += String(buf[i]);
+  }
+
+  Serial.print("cadena cifrada: ");
+  Serial.println(datos_cif);
+
+  datos_cif.toCharArray(bufcif, 50);
+  
+  //Desciframos los datos del cansat en la estacion de tierra
+  spritz_setup(&s_ctx, key, keyLen);
+  spritz_crypt(&s_ctx, bufcif, msgLen, bufcif);
+
+  /* Print MSG after decryption */
+  for (i = 0; i < msgLen; i++) {
+    Serial.write(bufcif[i]);
+  }
+  Serial.println(); 
 }
